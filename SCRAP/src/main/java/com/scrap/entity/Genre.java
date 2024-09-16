dbbc5103fda8fd59a6d3d9d27962271a305e3883
T.Nishigaki <wmg_gaki@ezweb.ne.jp>
2024-09-16 12:42:27 +0900

    add:【SCRAP】
    Entity追加

--- SCRAP/src/main/java/com/scrap/entity/Genre.java
+++ SCRAP/src/main/java/com/scrap/entity/Genre.java
@@ -1,46 +1,33 @@
 package com.scrap.entity;
 
-import java.sql.Timestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user", schema = "book")
+@Table(name = "user", schema = "genre")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Book {
+public class Genre {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "work_type")
+    private String work_type;
+
+    
     @Column(name = "name")
     private String name;
-
-    @ManyToOne()
-    @JoinColumn(name = "genre", referencedColumnName = "id")
-    private Genre genre; 
     
-    @Column(name = "author")
-    private String author;
-
-    @Column(name = "created")
-    private Timestamp created;
-
-    @Column(name = "modified")
-    private Timestamp modified;
-
     @Column(name = "delete_flag")
     private Boolean delete_Flag = false;
 }
